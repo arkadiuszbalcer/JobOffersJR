5aef94629d1a8d14d0e7ef84a4a403aa80fee4d2
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2024-12-11 18:49:21 +0100

    created offer module with tests

---
+++ src/main/java/com/junioroffers/domain/offer/OfferRepository.java
@@ -0,0 +1,9 @@
+package com.junioroffers.domain.offer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends MongoRepository<Offer, String> {
+    boolean existsByOfferUrl(String offerUrl);
+}

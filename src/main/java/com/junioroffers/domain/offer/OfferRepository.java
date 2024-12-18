fc0966c4f5b41076c8a3a6b508dca7dd5144c219
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2024-12-18 19:42:12 +0100

    added docker-compose, added  dependencies of integration test and springboot added

--- src/main/java/com/junioroffers/domain/offer/OfferRepository.java
+++ src/main/java/com/junioroffers/domain/offer/OfferRepository.java
@@ -3,7 +3,30 @@ package com.junioroffers.domain.offer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OfferRepository extends MongoRepository<Offer, String> {
     boolean existsByOfferUrl(String offerUrl);
+
+    Optional<Offer> findById(String id);
+
+    boolean existsById(String s);
+
+    <S extends Offer> S save(S entity);
+
+    List<Offer> findAllById(Iterable<String> strings);
+
+    long count();
+
+    void deleteById(String s);
+
+    void delete(Offer entity);
+
+    void deleteAllById(Iterable<? extends String> strings);
+
+    void deleteAll(Iterable<? extends Offer> entities);
+
+    void deleteAll();
 }
